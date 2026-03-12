f34b6770d4da18597e14a40415d6545ec7c920a6
kdy0618 <kdy0618@suwon.ac.kr>
2026-03-12 11:34:09 +0900

    4단계 - 게임 오버 및 리셋 1. 게임 오버 => 20번 놓치면 "GameOver" 메시지 출력 => 모든 버튼을 비활성화 처리 2. '다시 시작' 버튼 구현 => 게임 관련 모든 정보 초기화 => 게임 다시 시

--- Form1.cs
+++ Form1.cs
@@ -2,16 +2,54 @@ namespace CatchButton
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
         int score = 0;
         int randX = 0;
         int randY = 0;
+        int count = 0;
+        Size initsize;
+        public Form1()
+        {
+            InitializeComponent();
+            initsize = btnTarget.Size;
+        }
+        private void initiateGame()
+        {
+            //버튼 활성화
+            if(!btnTarget.Enabled) btnTarget.Enabled = true;
+            // 버튼 크기 초기화
+            btnTarget.Size = initsize;
+            // 점수 및 카운트 초기화
+            score = 0;
+            count = 0;
+            // 제목 업데이트
+            updateTitle();
+
+        }
+
         private void btnTarget_MouseEnter(object sender, EventArgs e)
         {
+            // 점수 계산  => 10점 감점
+            if (score >= 10) score -= 10;
+            //놓친 카운트 증가
+            count++;
+            //제목 텍스트 변경
+            updateTitle();
+         
+
+            // 20번 놓치면 게임 종료
+            if (count >= 20)
+            {
+                btnTarget.Enabled = false;
+                DialogResult result = MessageBox.Show("GameOver!\n다시 시작하시겠습니까?","게임 종료",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question
+                    );
+                if (result == DialogResult.Yes) {
+                    initiateGame();
+                }
+                return;
+            }
+
             //효과음 추가
             System.Media.SystemSounds.Asterisk.Play();
 
@@ -28,11 +66,7 @@ namespace CatchButton
             // 위치 이동
             btnTarget.Location = new Point(randX, randY);
 
-            // 점수 계산 및 제목 업데이트 => 10점 감점
-            if(score >= 10) score -= 10;
-
-            //제목 텍스트 변경
-            updateTitle();
+            
         }
 
         private void btnTarget_Click(object sender, EventArgs e)
@@ -46,13 +80,13 @@ namespace CatchButton
             btnTarget.Size = new Size(
                 (int)(btnTarget.Size.Width * 0.9), (int)(btnTarget.Size.Height * 0.9));
             //메시지 박스
-            MessageBox.Show("축하합니다~!");
+            MessageBox.Show("축하합니다~!","성공",MessageBoxButtons.OK,MessageBoxIcon.Information);
 
         }
         // 제목 텍스트 업데이트
         private void updateTitle()
         {
-            this.Text = $"버튼 위치: ({randX}, {randY}) 점수 : {score}";
+            this.Text = $"버튼 위치: ({randX}, {randY}) 점수: {score} 놓친 횟수: {count}/20";
         }
 
     }
