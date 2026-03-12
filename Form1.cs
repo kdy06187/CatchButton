31194fcdb0358e6d32fdae6335ae4a31cbc73690
kdy0618 <kdy0618@suwon.ac.kr>
2026-03-12 10:35:09 +0900

    1단계 - 기본 도망 기능 1. UI 구성 - button의 Text를“나를잡아봐＂로 설정 2. 버튼 이동기능구현 - button의 MouseEnter 이벤트 설정(random 좌표) 3. 버튼 위치정보표시 - 폼제목을 현재버튼의X, Y 좌표로 설정 4. 버튼 이동범위제한 - 버튼이 화면밖으로나가는경우를방지.

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,24 @@ namespace CatchButton
         {
             InitializeComponent();
         }
+
+        private void btnTarget_MouseEnter(object sender, EventArgs e)
+        {
+            // 난수 생성기
+            Random rd = new Random();
+            // 영역 계산
+            int maxX = this.ClientSize.Width;
+            int maxY = this.ClientSize.Height;
+
+            // 랜덤 좌표
+            int randX = rd.Next(0, maxX);
+            int randY = rd.Next(0, maxY);
+
+            // 위치 이동
+            btnTarget.Location = new Point(randX, randY);
+            //제목 텍스트 변경
+            this.Text = $"버튼 위치: ({randX}, {randY})";
+
+        }
     }
 }
